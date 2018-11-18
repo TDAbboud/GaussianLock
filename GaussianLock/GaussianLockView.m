11035c884ce3c006a0f2a9ef7364dd55d873def8
tabboud <tabboud@palantir.com>
2018-11-18 17:50:18 -0500

    initial commit

---
+++ GaussianLock/GaussianLockView.m
@@ -0,0 +1,52 @@
+//
+//  GaussianLockView.m
+//  GaussianLock
+//
+//  Created by Tony Abboud on 11/18/18.
+//  Copyright © 2018 Tony Abboud. All rights reserved.
+//
+
+#import "GaussianLockView.h"
+
+@implementation GaussianLockView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
