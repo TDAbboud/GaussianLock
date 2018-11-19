449bf9b73061c0972659b5cdd7932cd335dbcb70
tabboud <tabboud@palantir.com>
2018-11-18 23:01:03 -0500

    initial screenshot and display

--- GaussianLock/GaussianLockView.swift
+++ GaussianLock/GaussianLockView.swift
@@ -7,38 +7,63 @@
 //
 
 import ScreenSaver
+import AppKit
 
 class GaussianLockView: ScreenSaverView {
-    
+
+    var image : NSImage?
+
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
+        loadImage()
     }
-    
+
     required init?(coder decoder: NSCoder) {
         super.init(coder: decoder)
+        loadImage()
     }
-    
-    override func animateOneFrame() {
-    }
-    
+
     override func draw(_ rect: NSRect) {
         super.draw(rect)
+
+        if let image = image {
+            image.draw(in: rect)
+        } else {
+            // default to red, so we know something went wrong
+            NSColor.red.setFill()
+            rect.fill()
+            NSColor.black.set()
+        }
     }
-    
-    override func startAnimation() {
-        super.startAnimation()
-    }
-    
-    override func stopAnimation() {
-        super.stopAnimation()
-    }
-    
+
     override var hasConfigureSheet: Bool {
         return false
     }
-    
+
     override var configureSheet: NSWindow? {
         return nil
     }
-    
+
+    public func captureScreenToPath(_ destination: URL) -> URL {
+        let img = CGDisplayCreateImage(CGMainDisplayID())
+        let dest = CGImageDestinationCreateWithURL(destination as CFURL, kUTTypePNG, 1, nil)
+        CGImageDestinationAddImage(dest!, img!, nil)
+        CGImageDestinationFinalize(dest!)
+        return destination
+    }
+
+    func captureScreenToData() -> NSData {
+        let img = CGDisplayCreateImage(CGMainDisplayID())
+        let storage = NSMutableData.init()
+        let dest = CGImageDestinationCreateWithData(storage, kUTTypePNG, 1, nil)
+        CGImageDestinationAddImage(dest!, img!, nil)
+        CGImageDestinationFinalize(dest!)
+        return storage
+    }
+
+    func loadImage() {
+        let data = self.captureScreenToData()
+        self.image = NSImage.init(data: data as Data)!
+        self.needsDisplay = true
+    }
 }
